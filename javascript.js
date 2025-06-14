6f3340c72a06660a4e7e59e0d7875642f417eb7d
aulline13 <polinapeeva@yandex.ru>
2025-06-14 22:33:44 +0800

    Add functions for getting computer and human choices

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
 const rock = "Rock";
 const paper = "Paper";
 const scissors = "Scissors";
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 100);
+    console.log(randomNumber);
+    if (randomNumber < 33) {
+        return rock;
+    } else if (randomNumber < 66) {
+        return paper;
+    } else {
+        return scissors;
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt();
+    choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+    return choice;
+} 
+
